1fca70eadfd0507032f8472c33b1c26d544e18c6
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-18 20:35:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [news, setNews] = useState();
+  const url = "https://news-app-api-pearl.vercel.app/fetch";
+
+  function fetchData() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setNews(data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={fetchData} className="bg-amber-500 px-4 py-2">
+        click
+      </button>
+      <h1>{news ? news.articles[0].authore : "Fetching..."}</h1>
+    </>
   );
 }
 
