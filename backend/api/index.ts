2af188fb72733d4ecdd12174ceb3f0e54be0d39c
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-18 22:29:03 +0530

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,5 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const corsOption = {
+  origin : "https://news-app-kohl-omega.vercel.app",
+  optionsSuccessStatus : 200
+};
+
+app.use(cors(corsOption));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
