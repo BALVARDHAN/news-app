56b2cb2cf1da6b53348da7d57801a27f442addb4
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-18 20:36:23 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,6 +7,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/fetch", async (res, res) => {
+  const url =
+    "https://newsapi.org/v2/everything?q=bitcoin&apiKey=346fc7b36bfc4d5ea9b38eebf510ba9a";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => res.send(JSON.stringify(data)));
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
