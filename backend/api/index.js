9ba0350c3a45fc540d90cb1a8eac69b652f57242
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-18 22:21:39 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const options = {
+const corsOptions = {
   origin: "https://news-app-kohl-omega.vercel.app",
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(options));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/fetch", async (res, res) => {
+app.get("/fetch", cors(corsOptions), async (res, res) => {
   const url =
     "https://newsapi.org/v2/everything?q=bitcoin&apiKey=346fc7b36bfc4d5ea9b38eebf510ba9a";
   fetch(url)
