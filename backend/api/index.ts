e436fa1d37ff3f75dd2837524d5067e6844a2f06
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-18 22:31:13 +0530

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -11,6 +11,14 @@ app.use(cors(corsOption));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/fetch", (res, res) => {
+  const url =
+    "https://newsapi.org/v2/everything?q=bitcoin&apiKey=346fc7b36bfc4d5ea9b38eebf510ba9a";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => res.send(JSON.stringify(data)));
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
